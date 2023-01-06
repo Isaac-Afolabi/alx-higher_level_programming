628d32bc800688e846343aaef6c16dc0ef36fd29
ISAAC <adenusijoseph0@gmail.com>
2023-01-05 16:14:27 -0800

    INITIAL COMMITs

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the sum of 1 and 2"""
-    from add_0 import add
+    # Import the add function from the add_0 module
+    add_0 = __import__("add_0")
+    add = add_0.add
 
+    # Define the variables a and b
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+
+    # Call the add function and print the result
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
